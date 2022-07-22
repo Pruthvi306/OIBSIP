85c4ecbd6669eabb14212f570277cc14c7cee24c
Pruthvi306 <109028623+Pruthvi306@users.noreply.github.com>
2022-07-22 21:03:45 +0530

    Update ATM_SYSTEM.java

--- ATM_SYSTEM.java
+++ ATM_SYSTEM.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package atm_system;
 
-/**
- *
- * @author jude
- */
 public class ATM_SYSTEM {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         splash Keypad=new splash();
         Keypad.setVisible(true);
@@ -36,7 +26,7 @@ public class ATM_SYSTEM {
         catch(Exception e){
         
         }
-        //Keypad.
+        
         
     }
     
